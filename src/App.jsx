ffcd18de834b782c2ec37e7fdbe2ed507fa52bb3
pereyrapascuala@gmail.com <pereyrapascuala@gmail.com>
2024-08-03 23:38:08 -0300

    comenzando

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+
+function App() {
+
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className='box m-4 p-4'>
+      <h1 className='title'>Info</h1>
+
+    </div>   
+  );
+}
+export default App;
+
